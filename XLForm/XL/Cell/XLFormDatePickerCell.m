9eab32823fe8f74a331910372cc51e43e685d541
Chris Stamper <christopherstamper@gmail.com>
2016-06-02 13:32:46 -0400

    Update XLFormDatePickerCell.m

--- XLForm/XL/Cell/XLFormDatePickerCell.m
+++ XLForm/XL/Cell/XLFormDatePickerCell.m
@@ -46,7 +46,6 @@
     [_datePicker addTarget:self action:@selector(datePickerValueChanged:) forControlEvents:UIControlEventAllEvents];
     
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 0.3 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
-        [_datePicker setDate:[NSDate dateWithTimeIntervalSinceNow:0] animated:true ];
         [_datePicker setCountDownDuration:_datePicker.countDownDuration];
         [self datePickerValueChanged:_datePicker];
     });
